33f37b45ed5c44ea6fd9bf9d014e6f6d4dd65ee4
porkostomus <porkostomus@mail.com>
2019-04-09 17:42:11 -0700

    create lein project

---
+++ src/wml/core.clj
@@ -0,0 +1,6 @@
+(ns wml.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/wml/core_test.clj
@@ -0,0 +1,7 @@
+(ns wml.core-test
+  (:require [clojure.test :refer :all]
+            [wml.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
